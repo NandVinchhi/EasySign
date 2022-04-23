292370a4917dbb5694fa424e172e15af12c777c4
NandVinchhi <nand.vinchhi@gmail.com>
2022-04-23 23:25:26 +0530

    init

--- components/lessons/SignRecognizing.jsx
+++ components/lessons/SignRecognizing.jsx
@@ -12,6 +12,8 @@ import {
 } from "@chakra-ui/react";
 
 export const SignRecognizing = (props) => {
+  const [stage, setStage] = useState(0);
+  const [answer, setAnswer] = useState("");
   return (
     <>
       <NavbarLanding />
@@ -22,21 +24,76 @@ export const SignRecognizing = (props) => {
             alt="Sign"
             w="xl"
           />
+
+          {stage == 0 && (
+            <>
           <Center><Text fontWeight="bold" fontSize="2xl">Enter the alphabet signed above.</Text></Center>
           
+
           <HStack>
 
           <Input
             placeholder="Enter your answer"
+
+            value={answer}
+            onChange={e => {
+              if (e.target.value.length <= 1){
+                setAnswer(e.target.value.toUpperCase());
+              }
+
+            }}
             size="lg"
             alignSelf="center"
           />
 
          
-          <Button size="lg" colorScheme="blue" alignSelf="center">
+          <Button onClick={() => {
+            if (answer == props.question.toUpperCase()){
+              setStage(1);
+            }
+            else {
+              setStage(2);
+            }
+          }} size="lg" colorScheme="blue" alignSelf="center">
             Submit
           </Button>
+
           </HStack>
+          </>
+          )}
+
+          {stage == 1 && (
+            <>
+          <Center><Text fontWeight="bold" color="teal.500" fontSize="2xl">Correct answer.</Text></Center>
+          
+
+          
+          <Button onClick={() => {
+            props.updateScore(2);
+          }} size="lg" colorScheme="blue" alignSelf="center">
+            Next
+          </Button>
+
+         
+          </>
+          )}
+
+          {stage == 2 && (
+            <>
+          <Center><Text fontWeight="bold" color="red.500" fontSize="2xl">Wrong answer. The correct answer was '{props.question.toUpperCase()}'</Text></Center>
+          
+
+          
+          <Button onClick={() => {
+            props.updateScore(0);
+          }} size="lg" colorScheme="blue" alignSelf="center">
+            Next
+          </Button>
+
+         
+          </>
+          )}
+
 
           <Progress width="xl" value={parseInt(100 * props.questionNumber / 11)} hasStripe />
           <Center><Text fontWeight="bold" color="blue.600" fontSize="2xl">{props.questionNumber}/11</Text></Center>
