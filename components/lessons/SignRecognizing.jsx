874e07108922bd0b6aab982ded881a2e44a17dd1
advikkabra <advikkabra25@gmail.com>
2022-04-23 13:30:38 +0530

    lesson 1

--- components/lessons/SignRecognizing.jsx
+++ components/lessons/SignRecognizing.jsx
@@ -1,6 +1,38 @@
 import * as React from "react";
 import { NavbarLanding } from "../navbar/NavbarLanding";
+import {
+  Image,
+  Center,
+  Stack,
+  Input,
+  Button,
+  Progress,
+} from "@chakra-ui/react";
 
 export const SignRecognizing = () => {
-  return <NavbarLanding />;
+  return (
+    <>
+      <NavbarLanding />
+      <Center>
+        <Stack padding="4" spacing="6" marginTop="3rem">
+          <Image
+            src="http://www.milwaukeeindependent.com/wp-content/uploads/2019/06/062719_SignLanguage_01.jpg"
+            alt="Sign"
+            w="xl"
+          />
+          <Input
+            placeholder="Enter your alphabet"
+            size="lg"
+            alignSelf="center"
+          />
+
+          <Button colorScheme="blue" size="md" alignSelf="center">
+            Submit
+          </Button>
+
+          <Progress width="xl" value={20} hasStripe />
+        </Stack>
+      </Center>
+    </>
+  );
 };
